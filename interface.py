7c7f980ca8262a1d6d088bdddbfdc460eaeba597
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-24 15:01:02 -0700

    update defualt

--- interface.py
+++ interface.py
@@ -7,9 +7,23 @@ sl.title("Weather Forecast")
 place = sl.text_input("Place: ")
 days = sl.slider("Forecast Days", min_value=1, max_value=5,help="select the number of forecasted days")
 option = sl.selectbox("Data to view", ("Temperature", "Sky"))
+
+if len(place) == 0:
+    place = "Tokyo"
+
 sl.subheader(f"{option} for the next {days} days in {place}")
 
-dates, temp = get_data(place, days, option)
+filtered = get_data(place, days, option)
 
-figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
-sl.plotly_chart(figure)
+if option == "Temperature":
+    temp = [i["main"]["temp"] for i in filtered]
+    print(filtered)
+    dates = [i["dt_txt"] for i in filtered]
+    figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
+    sl.plotly_chart(figure)
+
+if option == "Sky":
+    filtered = [i["weather"][0]["main"] for i in filtered]
+
+
+
