a6f82bd3b3953a82f32e7cd5b00f226dde9ee19a
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-24 13:15:33 -0700

    arguments change

--- interface.py
+++ interface.py
@@ -1,14 +1,7 @@
 import streamlit as sl
 import pandas as pd
 import plotly.express as px
-
-
-def get_data(days):
-    dates = ["2022", "2023"]
-    temp = [10, 11]
-    temp = [days * i for i in temp]
-    return dates, temp
-
+from backend import get_data
 
 sl.title("Weather Forecast")
 place = sl.text_input("Place: ")
@@ -16,7 +9,7 @@ days = sl.slider("Forecast Days", min_value=1, max_value=5,help="select the numb
 option = sl.selectbox("Data to view", ("Temperature", "Sky"))
 sl.subheader(f"{option} for the next {days} days in {place}")
 
-dates, temp = get_data(days)
+dates, temp = get_data(place, days, option)
 
 figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
 sl.plotly_chart(figure)
